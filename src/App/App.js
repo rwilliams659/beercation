0cc9696469eb34a0405db1507107107ed4c0a6e6
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-17 12:09:21 -0600

    Create getSearchResults method nad pass to SearchForm

--- src/App/App.js
+++ src/App/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm'
+import ApiCalls from '../helpers/ApiCalls'
 
 class App extends Component {
   constructor() {
     super()
+    this.apiCalls = new ApiCalls();
     this.state = {
       searchResults: [],
       filteredSearchResults: [],
@@ -13,14 +15,27 @@ class App extends Component {
       breweriesToVisit: [],
       breweriesVisited: []
     }
+  }
 
+  getSearchResults = (searchTerm) => {
+    Promise.all([
+      this.apiCalls.fetchSearchResults(searchTerm, 1),
+      this.apiCalls.fetchSearchResults(searchTerm, 2)
+    ])
+      .then(([response1, response2]) => {
+        const searchResults = response1.concat(response2)
+        this.setState({ searchResults})
+      })
+      .catch(error => this.setState({ error }))
   }
 
   render() {
     return (
       <section className="App">
         <Header />
-        <SearchForm />
+        <SearchForm 
+          getSearchResults={this.getSearchResults}
+        />
       </section>
     );
   }

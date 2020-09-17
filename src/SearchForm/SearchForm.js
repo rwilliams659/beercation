d69e5fd34ff7dffbddd6200743dd8eff1ddb2dd3
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-17 12:09:34 -0600

    Call getSearchResults on form click

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import './SearchForm.css'
+import propTypes from 'prop-types'
 
 class SearchForm extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      error: '',
-      searchTerm: ''
+      // error: '',
+      searchTerm: '',
     }
   }
 
   handleChange = event => {
     const searchTerm = event.target.value
     this.setState({ searchTerm })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.getSearchResults(this.state.searchTerm); 
   }
 
   render() {
@@ -25,11 +31,15 @@ class SearchForm extends Component {
         <form className='city-search-form'>
           <label htmlFor='search'> Search the city you plan to visit below to see a list of breweries, bars, and brewpubs in the area.</label>
           <input id='search' type='text' placeholder='Enter a city name' value={this.state.searchTerm} onChange={this.handleChange}/>
-          <input type='submit' value='Search'/>
+          <input type='submit' value='Search' onClick={this.handleSubmit}/>
         </form>
       </section>
     )
   }
+}
+
+SearchForm.propTypes = {
+  getSearchResults: propTypes.func.isRequired
 }
 
 export default SearchForm

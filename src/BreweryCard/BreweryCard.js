42137d9e5be889f58a00e9565eca7b7397300640
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-17 16:42:55 -0600

    Create skeleton of BreweryCard component

---
+++ src/BreweryCard/BreweryCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './BreweryCard.css'
+import propTypes from 'prop-types'
+
+const BreweryCard = () => {
+  return (
+
+  )
+}
+
+BreweryCard.propTypes = {
+  
+}
+
+export default BreweryCard

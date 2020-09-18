f40b1d3c388e7b7cb3f8816580a9909b2193868b
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-17 18:17:17 -0600

    Add buttons below each card for users adding breweries to lists

--- src/BreweryCard/BreweryCard.js
+++ src/BreweryCard/BreweryCard.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import './BreweryCard.css'
 import propTypes from 'prop-types'
+import bottleCapIcon from '../images/bottle-cap.png'
 
-const BreweryCard = () => {
+
+const BreweryCard = ({name}) => {
   return (
-
+    <>
+    <article className='BreweryCard'>
+      <img src={bottleCapIcon} alt='bottle cap'/>
+      <h3>{name}</h3>
+    </article>
+    <section className='add-btns'>
+      <button className='to-visit-btn'>Mark as 'To Visit'</button>
+      <button className='visited-btn'>Mark as 'Visited'</button>
+    </section>
+    </>
   )
 }
 
 BreweryCard.propTypes = {
-  
+  name: propTypes.string.isRequired
 }
 
 export default BreweryCard

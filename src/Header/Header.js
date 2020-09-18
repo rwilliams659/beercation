7c8b0ce908f7aec03689404b295f203f17cdc05e
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-18 11:17:25 -0600

    Add link to H1

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Header.css'
 import logo from '../images/barrel.png'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
-      <section className='header-logo'>
-        <h1>BEERCATION</h1>
-        <img src={logo} alt='BEERCATION'/>
-      </section>
+      <Link to='/'>
+        <section className='header-logo'>
+          <h1>BEERCATION</h1>
+          <img src={logo} alt='BEERCATION'/>
+        </section>
+      </Link>
       <nav>
         <NavLink to='/' className='nav-link'>HOME</NavLink>
         <NavLink to='/to-visit' className='nav-link'>TO VISIT</NavLink>

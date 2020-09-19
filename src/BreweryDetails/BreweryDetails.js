c63a8211e7999b4a6a54c02195e359d29856ceb0
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-19 14:13:20 -0600

    Convert BreweryDetails to class component

--- src/BreweryDetails/BreweryDetails.js
+++ src/BreweryDetails/BreweryDetails.js
@@ -1,43 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './BreweryDetails.css'
 import propTypes from 'prop-types'
 import brewery1 from '../images/brewery1.jpg'
 import { Link } from 'react-router-dom'
 
-const BreweryDetails = ({brewery, toggleBreweryToUserList, breweriesVisited, breweriesToVisit}) => {
+class BreweryDetails extends Component {
+  componentDidMount() {
+    
+  }
 
-  const inBreweriesToVisit = breweriesToVisit.find(savedBrewery => savedBrewery.id === brewery.id);
+  render() {
+    const inBreweriesToVisit = this.props.breweriesToVisit.find(savedBrewery => savedBrewery.id === this.props.brewery.id);
 
-  const inBreweriesVisited = breweriesVisited.find(savedBrewery => savedBrewery.id === brewery.id);
+    const inBreweriesVisited = this.props.breweriesVisited.find(savedBrewery => savedBrewery.id === this.props.brewery.id);
 
-  return (
-    <section className='BreweryDetails'>
-      <section className='img-column'>
-        <button><Link to='/'>Back to Results</Link></button>
-        <img src={brewery1} alt='brewery'/>
-        <div>
-          {inBreweriesToVisit ? <p className='tag1'>To Visit</p> : ''}
-          {inBreweriesVisited ? <p className='tag2'>Visited</p> : ''}
-        </div>
-      </section>
-      <section className='brewery-info'>
-        <h2>{brewery.name}</h2>
-        <p>Address:<br/>{brewery.street}<br/>{brewery.city}, {brewery.state} {brewery.postal_code}</p>
-        <p>Telephone: {brewery.phone}</p>
-        <a href={brewery.website_url} target='_blank'>Learn more</a>
-        <section className='add-btns'>
-          {inBreweriesToVisit ?
-            <button className='to-visit-btn' onClick={() => toggleBreweryToUserList(brewery.id, 'breweriesToVisit')}>Unmark as To Visit</button> :
-            <button className='to-visit-btn' onClick={() => toggleBreweryToUserList(brewery.id, 'breweriesToVisit')}>Mark as To Visit</button>
-          }
-          {inBreweriesVisited ?
-            <button className='visited-btn' onClick={() => toggleBreweryToUserList(brewery.id, 'breweriesVisited')}>Unmark as Visited</button> :
-            <button className='visited-btn' onClick={() => toggleBreweryToUserList(brewery.id, 'breweriesVisited')}>Mark as Visited</button>
-          }
+    return (
+      <section className='BreweryDetails'>
+        <section className='img-column'>
+          <button><Link to='/'>Back to Results</Link></button>
+          <img src={brewery1} alt='brewery'/>
+          <div>
+            {inBreweriesToVisit ? <p className='tag1'>To Visit</p> : ''}
+            {inBreweriesVisited ? <p className='tag2'>Visited</p> : ''}
+          </div>
+        </section>
+        <section className='brewery-info'>
+          <h2>{this.props.brewery.name}</h2>
+          <p>Address:<br />{this.props.brewery.street}<br />{this.props.brewery.city}, {this.props.brewery.state} {this.props.brewery.postal_code}</p>
+          <p>Telephone: {this.props.brewery.phone}</p>
+          <a href={this.props.brewery.website_url} target='_blank' rel='noopener noreferrer'>Learn more</a>
+          <section className='add-btns'>
+            {inBreweriesToVisit ?
+              <button className='to-visit-btn' onClick={() => this.props.toggleBreweryToUserList(this.props.brewery.id, 'breweriesToVisit')}>Unmark as To Visit</button> :
+              <button className='to-visit-btn' onClick={() => this.props.toggleBreweryToUserList(this.props.brewery.id, 'breweriesToVisit')}>Mark as To Visit</button>
+            }
+            {inBreweriesVisited ?
+              <button className='visited-btn' onClick={() => this.props.toggleBreweryToUserList(this.props.brewery.id, 'breweriesVisited')}>Unmark as Visited</button> :
+              <button className='visited-btn' onClick={() => this.props.toggleBreweryToUserList(this.props.brewery.id, 'breweriesVisited')}>Mark as Visited</button>
+            }
+          </section>
         </section>
       </section>
-    </section>
-  )
+    )
+  }
 }
 
 BreweryDetails.propTypes = {

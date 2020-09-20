510f08e4f4520d8772f8ad3bd0f45d7b79ab3486
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-20 11:43:09 -0600

    Add tests for all navigation

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,13 +2,63 @@ import React from 'react';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 import { fetchSearchResults, fetchBreweryByName } from '../helpers/apiCalls'
 jest.mock('../helpers/apiCalls.js')
-import { MemoryRouter } from 'react-router-dom';
 
 describe('App', () => {
-  it('should')
+  it('should bring users to the correct views when using the navigation for Home and To Visit', () => {
 
-  //write out all the types of tests I need before I implement! Should be a TON; start by first going through each component 1 by 1
-  //then add user stories 
+    render(
+      <MemoryRouter>
+        < App />
+      </MemoryRouter>
+    )
+
+    const toVisitLink = screen.getByRole('link', { name: 'TO VISIT' });
+    const homeLink = screen.getByRole('link', { name:'HOME' });
+
+    fireEvent.click(toVisitLink);
+
+    const toVisitHeading = screen.getByRole('heading', { name: 'Breweries To Visit'})
+
+    expect(toVisitHeading).toBeInTheDocument(); 
+
+    fireEvent.click(homeLink);
+
+    const homeHeading = screen.getByRole('heading', { name: 'Welcome to Beercation!'});
+
+    expect(homeHeading).toBeInTheDocument();
+  });
+
+  it('should bring users to the correct views when using the navigation for Visited, plus the logo and h1', () => {
+
+    render(
+      <MemoryRouter>
+        < App />
+      </MemoryRouter>
+    )
+
+    const visitedLink = screen.getByRole('link', { name: 'VISITED' });
+    const logoLink = screen.getByAltText('BEERCATION');
+    const siteNameLink = screen.getByRole('heading', { name: 'BEERCATION' });
+
+    fireEvent.click(logoLink);
+
+    const homeHeading = screen.getByRole('heading', { name: 'Welcome to Beercation!' }); 
+
+    expect(homeHeading).toBeInTheDocument();
+
+    fireEvent.click(visitedLink);
+
+    const visitedHeading = screen.getByRole('heading', { name: 'Breweries Visited' });
+
+    expect(visitedHeading).toBeInTheDocument();
+
+    fireEvent.click(siteNameLink);
+
+    const homeView = screen.getByRole('heading', { name: 'Welcome to Beercation!' }); 
+
+    expect(homeView).toBeInTheDocument(); 
+  });
 })
